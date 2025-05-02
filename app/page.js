e9afcf97e72d73e7478b6e259612091a732a7688
Shadman Sakib <sadmansakib10000@gmail.com>
2025-05-02 17:01:14 +0600

    misc

--- app/page.js
+++ app/page.js
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
     <>
-      <h1 className="font-sans text-4xl">Hey!</h1>
-      <p className="font-mono">Build in Progress...</p>
+      <div className="container mx-auto flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-2xl font-bold text-warning">Hey!</h1>
+        <p className="font-mono pb-[10rem]">Build in Progress...</p>
+
+        <a
+          href="#"
+          className="text-primary text-xs font-mono capitalize hover:underline underline-offset-2"
+        >
+          Login to test out what's coming next!
+        </a>
+      </div>
     </>
   );
 }
